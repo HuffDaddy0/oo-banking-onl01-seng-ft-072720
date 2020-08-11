53781e178767fbf2bf2b03a24895971455cf8e04
Tyler Huffman <ty.huff24@gmail.com>
2020-08-11 22:53:33 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -24,7 +24,7 @@ def execute_transaction
     if @sender.balance > @amount && @status == "pending"
       @sender.balance = @sender.balance - amount
       @receiver.balance = @receiver.balance + amount
-      self.status = "complete"
+      @status = "complete"
     else
       self.status = "rejected"
       "Transaction rejected. Please check your account balance."
